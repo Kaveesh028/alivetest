213c6d40bb39d759874847f48297e7c392db1aa7
Kaveesh028 <130683460+Kaveesh028@users.noreply.github.com>
2023-10-05 18:12:44 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const keep_alive = require('./keep_alive.js')
+
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
